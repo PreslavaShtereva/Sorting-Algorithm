3017c2f61f46e7b0392634c94de37183062a6394
Preslava Shtereva <127541105+PreslavaShtereva@users.noreply.github.com>
2024-10-17 11:00:18 +0300

    Create selection_sort.c

---
+++ selection_sort.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define ARR_LEN 10
+ 
+void selection_sort(int arr[]);
+ 
+int main() {
+	int arr[ARR_LEN] = { 7, 21, 3, 47, 101, 9, 55, 8, 41, 31 };
+ 
+	for (int i = 0; i < ARR_LEN; i++)
+		printf("%d ", arr[i]);
+ 
+	printf("\n");
+ 
+	selection_sort(arr);
+ 
+	return 0;
+}
+ 
+void selection_sort(int arr[]) {
+	for (int i = 0; i < ARR_LEN - 1; i++) { 
+		int min_idx = i; // Assume the current element is the minimum.
+
+		for (int j = i + 1; j < ARR_LEN; j++) {
+			if (arr[j] < arr[min_idx]) {
+				min_idx = j; 
+			}
+		}
+ 
+		if (min_idx != i) {
+			int temp = arr[i];
+			arr[i] = arr[min_idx];
+			arr[min_idx] = temp;
+		}
+	}
+ 
+
+	for (int i = 0; i < ARR_LEN; i++)
+		printf("%d ", arr[i]);
+ 
+	printf("\n");
+}
